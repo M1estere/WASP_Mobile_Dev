74f42ed1feed7d6b6f709627a7f192f256be1511
Ilya_Solov <58213582+M1estere@users.noreply.github.com>
2022-12-03 21:17:09 +0300

    Update MainPage.xaml.cs

--- Practice/Pin_Code/MAUI_Calculator/MainPage.xaml.cs
+++ Practice/Pin_Code/MAUI_Calculator/MainPage.xaml.cs
@@ -21,37 +21,37 @@ public partial class MainPage : ContentPage
 
 	private void ActionClicked(object sender, EventArgs e)
 	{
-        _previousValue = Convert.ToDouble(DisplayLabel.Text.Split('\n')[0]);
+        	_previousValue = Convert.ToDouble(DisplayLabel.Text.Split('\n')[0]);
 		_action = Convert.ToChar((sender as Button).Text);
 		DisplayLabel.Text += '\n';
-    }
+    	}
 
 	private void ClearClicked(object sender, EventArgs e)
 	{
-        SetLabelAlignment(TextAlignment.End);
+        	SetLabelAlignment(TextAlignment.End);
 
-        DisplayLabel.Text = "";
-    }
+		DisplayLabel.Text = "";
+    	}
 
 	private void ConfirmClicked(object sender, EventArgs e)
 	{
-        if (DisplayLabel.Text.ToString() == PASSWORD)
+        	if (DisplayLabel.Text.ToString() == PASSWORD)
 		{
 			DisplayLabel.Text = "Congratulations, you are in!";
-            SetLabelAlignment(TextAlignment.Center);
-            foreach (View a in ((sender as Button).Parent as Grid).Children.Cast<View>())
-            {
-                a.IsEnabled = false;
-            }
-        }
+            		SetLabelAlignment(TextAlignment.Center);
+            		foreach (View a in ((sender as Button).Parent as Grid).Children.Cast<View>())
+            		{
+                		a.IsEnabled = false;
+            		}
+        	}
 		else 
 			DisplayLabel.Text = "";
 	}
 
 	private void SetLabelAlignment(TextAlignment _alignment)
 	{
-        DisplayLabel.VerticalTextAlignment = _alignment;
-        DisplayLabel.HorizontalTextAlignment = _alignment;
-    }
+        	DisplayLabel.VerticalTextAlignment = _alignment;
+        	DisplayLabel.HorizontalTextAlignment = _alignment;
+    	}
 }
 
